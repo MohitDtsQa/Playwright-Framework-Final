e8c854f8dfa8e49655cf9b02856413934bb7dfc5
Mohit <mohitdynatech@gmail.com>
2026-02-12 16:07:07 +0530

    Added tags in ui

--- Tests/Add-Remove_Elements.spec.ts
+++ Tests/Add-Remove_Elements.spec.ts
@@ -1,14 +1,15 @@
 import * as _ from '@Index';
 import { AddRemoveEle } from '@Add-Remove_Elements.page';
 
+_.test.describe('AddRemove_Element @ui', () => {
+    _.test('AddRemove_Element', async ({ page }) => {
 
-_.test('AddRemove_Element', async ({ page }) => {
-    
-    const basePage = new _.BasePage(page);
-    const addRemoveEle = new AddRemoveEle(page);
+        const basePage = new _.BasePage(page);
+        const addRemoveEle = new AddRemoveEle(page);
 
-    await basePage.goToUrl();
-    await addRemoveEle.ClickAddRemoveEleLink();
-    await addRemoveEle.ClickAddElementButton();
-    await addRemoveEle.ClickDeleteButton();
+        await basePage.goToUrl();
+        await addRemoveEle.ClickAddRemoveEleLink();
+        await addRemoveEle.ClickAddElementButton();
+        await addRemoveEle.ClickDeleteButton();
+    });
 });
--- Tests/StatusCode.spec.ts
+++ Tests/StatusCode.spec.ts
@@ -1,21 +1,23 @@
 import * as _ from '@Index';
 import { StatusCode } from '@StatusCode.page';
 
-_.test('Status Code', async ({ page }) => {
-    const basePage = new _.BasePage(page);
-    const statusCode = new StatusCode(page);
+_.test.describe('Status Code @ui', () => {
+    _.test('Status Code', async ({ page }) => {
+        const basePage = new _.BasePage(page);
+        const statusCode = new StatusCode(page);
 
-    await basePage.goToUrl();
-    await statusCode.ClickstatusCodeLink();
-    await statusCode.ClickCode200Text();
-    await statusCode.GetStatusCode(200);
-    await page.goBack();
-    await statusCode.ClickCode301Text();
-    await statusCode.GetStatusCode(300);
-    await page.goBack();
-    await statusCode.ClickCode404Text();
-    await statusCode.GetStatusCode(404);
-    await page.goBack();
-    await statusCode.ClickCode500Text();
-    await statusCode.GetStatusCode(500);
-})
+        await basePage.goToUrl();
+        await statusCode.ClickstatusCodeLink();
+        await statusCode.ClickCode200Text();
+        await statusCode.GetStatusCode(200);
+        await page.goBack();
+        await statusCode.ClickCode301Text();
+        await statusCode.GetStatusCode(300);
+        await page.goBack();
+        await statusCode.ClickCode404Text();
+        await statusCode.GetStatusCode(404);
+        await page.goBack();
+        await statusCode.ClickCode500Text();
+        await statusCode.GetStatusCode(500);
+    });
+});
